04d801b3b88e5fbf96a2d4bbaba720210f28e416
Ayyala Somayajula Harshavardhan <benharsha10@gmail.com>
2026-01-06 20:17:15 +0530

    Update 0231-power-of-two.java

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -2,7 +2,10 @@ class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n<=0)
         return false;
-        // if n is a power of two its binary rep has the first digit as 1 followed by zeroes, and n-1 has the first digit as 0 followed by ones, so performing bitwise AND will give us 0 if it is indeed a power of 2
+        /* if n is a power of two its binary rep has the first digit as 1 followed by zeroes, 
+        and n-1 has the first digit as 0 followed by ones, 
+        so performing bitwise AND will give us 0 if it is indeed a power of 2
+        */
         if((n & n-1) == 0)
         return true;
         else
