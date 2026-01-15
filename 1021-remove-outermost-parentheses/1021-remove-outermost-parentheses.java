3de70e5039e090770a9604a31ca58bdf2d1cd13d
Ayyala Somayajula Harshavardhan <benharsha10@gmail.com>
2026-01-15 12:12:18 +0530

    Time: 4 ms (58.29%), Space: 43.3 MB (93.39%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder sb = new StringBuilder();
+        int i;
+        int n = s.length();
+        int count = 0;
+        for(i = 0; i<s.length()-1; i++){
+            if(s.charAt(i) == '('){
+                if(count > 0){
+                sb.append(s.charAt(i));
+                count++;
+                }
+                else
+                count++;
+            }
+            else
+            {
+               count--;
+               if(count == 0)
+               continue;
+               else
+               sb.append(s.charAt(i));
+            }
+        }
+        String res = sb.toString();
+        return res;
+        
+    }
+}
