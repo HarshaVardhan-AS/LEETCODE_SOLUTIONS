a524735faff7f7762de79f6c0a0383d467736ca5
Ayyala Somayajula Harshavardhan <benharsha10@gmail.com>
2026-01-18 09:43:21 +0530

    Time: 2 ms (18.54%), Space: 43.8 MB (45.48%) - LeetHub

---
+++ 3813-vowel-consonant-score/3813-vowel-consonant-score.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int vowelConsonantScore(String s) {
+        int count = 0;
+        HashSet<Character> set = new HashSet<>();
+        set.add('a');
+        set.add('e');
+        set.add('i');
+        set.add('o');
+        set.add('u');
+        int n = s.length();
+        int i;
+        int v = 0;
+        int c = 0;
+        int score = 0;
+        for(i = 0; i<n; i++){
+            char ch = s.charAt(i);
+            if(set.contains(ch))
+                v++;
+            else if((!Character.isDigit(ch)) && (ch!=' '))
+                c++;
+            if(c>0)
+                score = (int)Math.floor(v/c);
+        }
+        return score;
+        
+    }
+}
