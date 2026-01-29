130d4fbcce5a104f09de93a3118594c0a84569b7
Ayyala Somayajula Harshavardhan <benharsha10@gmail.com>
2026-01-29 20:28:41 +0530

    Time: 9 ms (7.32%), Space: 46.3 MB (19.48%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        double res = Math.log10(n)/Math.log10(3);
+        if(res%1 == 0.0)
+        return true;
+        else
+        return false;
+    }
+}
