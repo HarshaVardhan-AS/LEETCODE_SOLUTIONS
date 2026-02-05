90b1a09154ddbeb6286f32a6c7c1f16d7b06f452
Ayyala Somayajula Harshavardhan <benharsha10@gmail.com>
2026-02-05 20:37:22 +0530

    Time: 7 ms (40.24%), Space: 48.3 MB (9.47%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,6 +1,5 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        int n = candidates.length;
         Arrays.sort(candidates);
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
@@ -9,21 +8,20 @@ class Solution {
         
     }
     public static void solve(int idx, int[] arr, int target, List<List<Integer>> res, List<Integer> temp){
-       
-        if(target == 0){
-            res.add(new ArrayList<>(temp));
-            return;
-        }
-         if(idx == arr.length){
-            return;
-        }
-        if(arr[idx] > target)
+       if((target == 0)){
+        res.add(new ArrayList<>(temp));
         return;
-        temp.add(arr[idx]);
-        solve(idx+1, arr, target - arr[idx], res, temp);
-        temp.remove(temp.size()-1);
-        while((idx < arr.length-1)&&(arr[idx] == arr[idx+1] ))
-        idx++;
-        solve(idx+1, arr, target, res, temp);
+       }
+       if(idx == arr.length)
+       return;
+       if(arr[idx] > target)
+       return;
+       temp.add(arr[idx]);
+       solve(idx+1, arr, target-arr[idx], res, temp);
+       temp.remove(temp.size()-1);
+       while((idx < arr.length-1) && (arr[idx] == arr[idx+1]))
+       idx++;
+       solve(idx+1, arr, target, res, temp);
+       
     }
 }
