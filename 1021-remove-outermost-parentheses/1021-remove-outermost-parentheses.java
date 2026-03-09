0884ea8018d5d964d76063e0f5d9a23155645765
Ayyala Somayajula Harshavardhan <benharsha10@gmail.com>
2026-03-09 22:14:20 +0530

    Time: 3 ms (61.7%), Space: 43.6 MB (70.68%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -4,25 +4,24 @@ class Solution {
         int i;
         int n = s.length();
         int count = 0;
-        for(i = 0; i<s.length()-1; i++){
+        for(i = 0; i<n; i++){
             if(s.charAt(i) == '('){
-                if(count > 0){
+                count++;
+                if(count == 1)
+                continue;
+                
                 sb.append(s.charAt(i));
-                count++;
-                }
-                else
-                count++;
+                
             }
             else
             {
-               count--;
-               if(count == 0)
-               continue;
-               else
-               sb.append(s.charAt(i));
+                count--;
+                if(count == 0)
+                continue;
+                else
+                sb.append(s.charAt(i));
             }
         }
-        
         return sb.toString();
         
     }
