84e085e8af53e8229a241d24e3aa3066bea5c6e6
Ayyala Somayajula Harshavardhan <benharsha10@gmail.com>
2026-03-17 22:11:14 +0530

    Time: 25 ms (15.39%), Space: 52.6 MB (22.8%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,34 +1,19 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
         int n = nums.length;
-        int res1 = 0;
-        int res2 = 0;
-        int left = 0;
-        int sum = 0;
-        for(int right = 0; right<n; right++){
-            sum = sum + nums[right];
-            while(sum > goal){
-                sum-=nums[left];
-                left++;
-            }
-            res1+= (right-left +1);
+       HashMap<Integer, Integer> map = new HashMap<>();
+       int currsum = 0;
+       int res = 0;
+       map.put(0,1);
+       for(int i = 0; i <n; i++){
+        currsum+= nums[i];
+        int target = currsum - goal;
+        if(map.containsKey(target))
+        res+= map.get(target);
+        map.put(currsum, map.getOrDefault(currsum, 0)+1);
 
-        }
-        left = 0;
-        sum = 0;
-        if(goal == 0)
-        res2 = 0;
-        else{
-        for(int right = 0; right<n; right++){
-            sum = sum + nums[right];
-            while((sum > goal-1)){
-                sum-= nums[left];
-                left++;
-            }
-            res2+= (right-left+1);
-        }
-        }
-        return res1 - res2;
-        
+
+       }
+       return res;
     }
 }
