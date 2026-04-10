5547ec7f7d6ad841e7673093a5feea07cb93cdd3
Ayyala Somayajula Harshavardhan <benharsha10@gmail.com>
2026-04-10 22:56:56 +0530

    Time: 2 ms (86.04%), Space: 48 MB (48.6%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxindex = 0;
+        int currindex = 0;
+        int n = nums.length;
+        int i;
+        if(n == 1)
+        return true;
+        for(i = 0; i<n; i++){
+            currindex = i;
+            if(maxindex >= currindex){
+            maxindex = Math.max(maxindex, currindex + nums[i]);  
+            continue;
+            }
+            else
+            return false;
+        }
+        return true;
+        
+    }
+}
