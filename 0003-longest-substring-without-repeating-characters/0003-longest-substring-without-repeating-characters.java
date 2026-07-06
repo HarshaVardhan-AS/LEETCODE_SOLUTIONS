4221094cbaf903f7ccd384525e1271c08697d641
Ayyala Somayajula Harshavardhan <benharsha10@gmail.com>
2026-07-06 22:12:50 +0530

    Time: 6 ms (70.27%), Space: 46.3 MB (55.27%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -7,12 +7,12 @@ class Solution {
         int n = s.length();
         for(right = 0; right<n; right++){
             while(set.contains(s.charAt(right))){
-            set.remove(s.charAt(left));
-            left++;
+                set.remove(s.charAt(left));
+                left++;
             }
             set.add(s.charAt(right));
-            maxlength = Math.max(maxlength, right-left+1);
+            maxlength = Math.max(maxlength, right-left + 1);
         }
-         return(maxlength);
+        return maxlength;
     }
 }
