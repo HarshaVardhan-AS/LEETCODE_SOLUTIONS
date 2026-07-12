d322f2e4f5b154cee24f134bf1ef1ec66467f12d
Ayyala Somayajula Harshavardhan <benharsha10@gmail.com>
2026-07-12 21:05:42 +0530

    Time: 2 ms (92.23%), Space: 47.7 MB (56.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -5,10 +5,9 @@ class Solution {
         int i;
         for(i = 0; i<n; i++){
             if(nums[i]!=0){
-                
-                int temp = nums[j];
-                nums[j]= nums[i];
-                nums[i] = temp;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
                 j++;
             }
         }
