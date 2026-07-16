7589175153b3fe1924534311b917d42669f833fc
Ayyala Somayajula Harshavardhan <benharsha10@gmail.com>
2026-07-16 22:36:56 +0530

    Time: 0 ms (100%), Space: 43.8 MB (46.75%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -7,24 +7,22 @@ class Solution {
             int mid = low + (high-low)/2;
             if(nums[mid] == target)
             return mid;
-            if(nums[low]<=nums[mid]){
-                if(nums[low]<= target && target < nums[mid]){
+            if(nums[low]<= nums[mid]){
+                if(nums[low] <= target && target < nums[mid]){
                     high = mid-1;
                 }
-                else{
-                    low = mid+1;
-                }
+                else
+                low = mid+1;
             }
             else{
-                if(nums[mid]< target && target <= nums[high])
-                low = mid+1;
+                if(nums[mid] < target && target <= nums[high]){
+                    low = mid+1;
+                }
                 else
                 high = mid-1;
             }
-
-            
-        }
-        return -1;
-        
     }
+                return -1;
+    }
+
 }
